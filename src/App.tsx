49dd41ca8439d491232a10ddf6a3fd3db22434b6
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-05-17 14:46:01 +0600

    Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import SigninForm from "./_auth/forms/SigninForm";
 import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm.tsx";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
 
 const App = () => {
-  return <div className="text-2xl">App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/*private routes*/}
+        <Route element={<RootLayout />}></Route>
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,5 @@
+const SignupForm = () => {
+  return <div>SignUpForm</div>;
+};
+
+export default SignupForm;
