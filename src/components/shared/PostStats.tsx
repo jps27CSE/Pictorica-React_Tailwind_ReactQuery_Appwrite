6227500aa41d1ca95fc9c8d6c513fe6a9988d885
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-09-18 16:13:11 +0600

    Like and Save Section UI

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,36 @@
-const PostStats = ({ post, userId }) => {
-  return <div>PostStats</div>;
+import { Models } from "appwrite";
+
+type PostStatsProps = {
+  post: Models.Document;
+  userId: string;
+};
+
+const PostStats = ({ post, userId }: PostStatsProps) => {
+  return (
+    <div className="flex justify-between items-center z-20">
+      <div className="flex gap-2 mr-5">
+        <img
+          src="/assets/icons/like.svg"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer "
+          alt="like"
+        />
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+
+      <div className="flex gap-2 ">
+        <img
+          src="/assets/icons/save.svg"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer "
+          alt="like"
+        />
+      </div>
+    </div>
+  );
 };
 export default PostStats;
