c1de247de4214d90b21ea82e1f2599991c906b14
jps27CSE <jackpritomsoren@hotmail.com>
2024-09-29 12:58:16 +0600

    Post Details (Progress)

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,4 +1,59 @@
+import { Link, useParams } from "react-router-dom";
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations.ts";
+import Loader from "@/components/shared/Loader.tsx";
+import { multiFormatDateString } from "@/lib/utils.ts";
+
 const PostDetails = () => {
-  return <div>PostDetails</div>;
+  const { id } = useParams();
+  const { data: post, isPending } = useGetPostById(id || "");
+
+  return (
+    <div className="post_details-container">
+      {isPending ? (
+        <Loader />
+      ) : (
+        <div className="post_details-card">
+          <img src={post?.imageUrl} alt="post" className="post_details-img" />
+          <div className="post_details-info">
+            <div className="flex-between w-full">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3"
+              >
+                <img
+                  src={
+                    post?.creator?.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
+                  alt="creator"
+                  className="rounded-full w-12 lg:-12 "
+                />
+
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1 ">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular ">
+                      {multiFormatDateString(post?.$createdAt)}
+                    </p>
+                    -{" "}
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+              <div className="flex-center gap-4">
+                <Link to={`/update-post/${post?.$id}`}>
+                  <img src="/assets/icons/edit.svg" alt="edit" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 export default PostDetails;
